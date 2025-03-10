7ffa5ccf0c53f7ebc29c9afb185e15b174f169e4
Arifur Rahman <90108739+developerarif10@users.noreply.github.com>
2025-03-10 11:36:42 +0600

    Back Button added on Login & Register page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,11 +1,20 @@
+import { ChevronLeft } from "lucide-react";
 import { LoginForm } from "./_components/login-form";
 
+import Link from "next/link";
 import SocialLogins from "./_components/social-logins";
 
 const LoginPage = () => {
   return (
     <div className="w-full flex-col h-screen flex items-center justify-center">
       <div className="container">
+        <Link
+          href="/"
+          className="absolute top-4 left-4 flex items-center text-white bg-white/20 px-4 py-2 rounded-lg backdrop-blur-md transition-all duration-300 hover:bg-white/30 hover:scale-105 shadow-lg"
+        >
+          <ChevronLeft className="w-5 h-5 mr-2" />
+          Back
+        </Link>
         <LoginForm />
         <SocialLogins />
       </div>
--- app/register/[role]/page.jsx
+++ app/register/[role]/page.jsx
@@ -1,9 +1,18 @@
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
 import { SignupForm } from "../_components/signup-form";
 
 const RegisterPage = ({ params: { role } }) => {
   return (
     <div className="w-full flex-col h-screen flex items-center justify-center">
       <div className="container">
+        <Link
+          href="/"
+          className="absolute top-4 left-4 flex items-center text-white bg-white/20 px-4 py-2 rounded-lg backdrop-blur-md transition-all duration-300 hover:bg-white/30 hover:scale-105 shadow-lg"
+        >
+          <ChevronLeft className="w-5 h-5 mr-2" />
+          Back
+        </Link>
         <SignupForm role={role} />
       </div>
     </div>
